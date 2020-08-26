ca09b4271f48f8c14a90bff5debc23a427ea5e83
Felipe Rodrigues <lipe2008.lipao@gmail.com>
2020-08-26 03:10:23 -0300

    Criação do template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import { SelectComponent } from './content/select/select.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+      NavbarComponent,
+      CarouselComponent,
+      ContentComponent,
+      FooterComponent,
+      SelectComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
